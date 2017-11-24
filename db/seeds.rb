b74eb606ed46ae60119ef791c22fddf74c5e0691
raphael-theron <raphael.theron@gmail.com>
2017-11-24 19:28:17 +0100

    Initiate models and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Artist.destroy_all
+Song.destroy_all
+
+artist1 = Artist.create!( { name: "Bruno Mars"} )
+artist2 = Artist.create!( { name: "Beach boys"} )
+
+artist_count = Artist.count
+puts "#{artist_count} artists were created."
+
+
+
+song1 = Song.create!( { name: "Grenade", artist: artist1 } )
+song2 = Song.create!( { name: "Uptown Funk", artist: artist1 } )
+song3= Song.create!( { name: "Kokomo", artist: artist2 } )
+
+song_count = Song.count
+puts "#{song_count} songs were created."
